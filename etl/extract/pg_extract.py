fbca9307a9a7970208e5ca3e3b087af0a6c29793
vitaly <v.shchurov@yadro.com>
2022-10-22 20:42:09 +0300

    Added the extractor

---
+++ etl/extract/pg_extract.py
@@ -0,0 +1,42 @@
+import os
+import logging
+from contextlib import closing, contextmanager
+
+import psycopg2
+
+
+class PostgresExtractor:
+    """
+    A class for extracting data from a PostgreSQL database.
+    """
+
+    def __init__(self, chunk: int):
+        self.chunk = chunk
+        self.modified = ...
+        self.state = ...
+
+    def _connect(self):
+        auth = {'dbname': os.environ.get('DB_NAME'), 'user': os.environ.get('DB_USER'),
+                'password': os.environ.get('DB_PASSWORD'), 'host': os.environ.get('DB_HOST'),
+                'port': os.environ.get('DB_PORT'), 'options': '-c search_path=content'}
+        try:
+            cursor = psycopg2.connect(**auth, cursor_factory=DictCursor)
+            return cursor
+        except psycopg2.OperationalError:
+            logging.exception("Database transfer failed. Could not connect to the database server!")
+        except Exception as error:
+            logging.exception(f"Database transfer failed. The following exception came up: {error}")
+
+    def extract(self):
+
+        with closing(self._connect()) as pg_cursor:
+            with open('extract_query') as query_file:
+                query = query_file.read()
+
+        pg_cursor.execute(query, (self.modified, self.modified, self.modified))
+
+        while data := pg_cursor.fetchmany(self.chunk):
+            self.state.save_key('pg_state', data)
+            logging.info("Extracted film data from PostgreSQL")
+            yield data
+            self.sate.save_key('pg_state', None)
