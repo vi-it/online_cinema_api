a5233a9701bedc7277181bf8ea0e9f43b2533157
vitaly <v.shchurov@yadro.com>
2022-10-22 20:28:02 +0300

    Added query to the PostgresExtractor.extract method

--- etl/pg_extract.py
+++ etl/pg_extract.py
@@ -18,10 +18,45 @@ class PostgresExtractor:
                 'password': os.environ.get('DB_PASSWORD'), 'host': os.environ.get('DB_HOST'),
                 'port': os.environ.get('DB_PORT'), 'options': '-c search_path=content'}
         try:
-            with closing(psycopg2.connect(**auth, cursor_factory=DictCursor)) as pg_conn:
+            connection = psycopg2.connect(**auth, cursor_factory=DictCursor)
+            return connection
         except psycopg2.OperationalError:
             logging.exception("Database transfer failed. Could not connect to the database server!")
         except Exception as error:
             logging.exception(f"Database transfer failed. The following exception came up: {error}")
 
+    def extract(self):
+
+        with closing(self._connect()) as pg_connect:
+            query = """
+            SELECT
+               fw.id,
+               fw.title,
+               fw.description,
+               fw.rating,
+               fw.type,
+               fw.created,
+               fw.modified,
+               COALESCE (
+                   json_agg(
+                       DISTINCT jsonb_build_object(
+                           'person_role', pfw.role,
+                           'person_id', p.id,
+                           'person_name', p.full_name
+                       )
+                   ) FILTER (WHERE p.id is not null),
+                   '[]'
+               ) as persons,
+               array_agg(DISTINCT g.name) as genres
+            FROM content.film_work fw
+            LEFT JOIN content.person_film_work pfw ON pfw.film_work_id = fw.id
+            LEFT JOIN content.person p ON p.id = pfw.person_id
+            LEFT JOIN content.genre_film_work gfw ON gfw.film_work_id = fw.id
+            LEFT JOIN content.genre g ON g.id = gfw.genre_id
+            WHERE fw.modified > %s OR 
+            g.modified > %s OR 
+            p.modified > %s
+            GROUP BY fw.id
+            ORDER BY fw.modified;
+            """
 
