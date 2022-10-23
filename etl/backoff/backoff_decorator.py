74621a8334493b4b6e91230add0f5835526278ef
vitaly <v.shchurov@yadro.com>
2022-10-23 16:26:31 +0300

    Refactored the backoff decorator

--- etl/backoff/backoff_decorator.py
+++ etl/backoff/backoff_decorator.py
@@ -5,7 +5,8 @@ import typing
 
 
 def backoff(exceptions: tuple, start_sleep_time=0.1, factor=2,
-            border_sleep_time=10) -> typing.Any:
+            border_sleep_time=10, tries=5,
+            logger=logging.exception) -> typing.Any:
     """
     Функция для повторного выполнения функции через некоторое время, если
     возникла ошибка. Использует наивный экспоненциальный рост времени повтора
@@ -17,17 +18,21 @@ def backoff(exceptions: tuple, start_sleep_time=0.1, factor=2,
     :param start_sleep_time: начальное время повтора
     :param factor: во сколько раз нужно увеличить время ожидания
     :param border_sleep_time: граничное время ожидания
+    :param tries: количество попыток
+    :param logger: тип логирования при возникновении проблем
     :return: результат выполнения функции
     """
     def func_wrapper(func) -> typing.Any:
         @functools.wraps(func)
         def inner(*args, **kwargs):
             sleep_time = start_sleep_time
-            while True:
+            num_tries = tries
+            while num_tries > 0:
+                num_tries -= 1
                 try:
                     return func(*args, **kwargs)
                 except exceptions as err:
-                    logging.info(f"Error: occurred {err}")
+                    logger(f"Error: occurred {err}")
                     sleep_time *= factor
                     sleep_time = min(sleep_time, border_sleep_time)
                     time.sleep(sleep_time)
