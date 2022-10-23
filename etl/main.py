42f3fc96a0c8e326431bdd395660852a9dd1673e
vitaly <v.shchurov@yadro.com>
2022-10-23 10:14:21 +0300

    Refactored main.py

--- etl/main.py
+++ etl/main.py
@@ -2,30 +2,30 @@ from dotenv import load_dotenv
 
 import transform
 import upload
+from extract import PostgresExtractor
 
 
 load_dotenv()
 
 
+class PostgresToElastic:
+
+    def process_data(self):
+        extractor = PostgresExtractor()
+
+        for raw_data in extractor.extract():
+            for row in raw_data:
+                fw = transform.Transform(row)
+                film = fw.to_filmwork()
+
+                es_loader = upload.ElasticsearchLoader()
+                es_loader.upload_data(film)
+
+
 def main():
-    from extract import PostgresExtractor
-    extractor = PostgresExtractor()
+   pg_to_es = PostgresToElastic()
+   pg_to_es.process_data()
 
-    counter = 0
-    for raw_data in extractor.extract(chunk=10):
-        for row in raw_data:
-            counter += 1
-            fw = transform.Transform(row)
-            film = fw.to_filmwork()
-
-            import json
-            with open('testing', 'a') as file:
-                file.write(json.dumps(dict(film)))
-                file.write('\n')
-
-            es_loader = upload.ElasticsearchLoader()
-            es_loader.upload_data(film)
-    print(counter)
 
 if __name__ == '__main__':
     main()
