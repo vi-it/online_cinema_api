8bed125ca6096d67aceaef0c413ca21780b60608
vitaly <v.shchurov@yadro.com>
2022-10-29 23:19:09 +0300

    Added redis and es connections to main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import logging
 
-import uvicorn
+import aioredis
+import uvicorn as uvicorn
+from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
 from src.core import config
 from src.core.logger import LOGGING
+from src.db import elastic
+from src.db import redis
 
 app = FastAPI(
     title=config.PROJECT_NAME,
@@ -14,11 +18,36 @@ app = FastAPI(
     default_response_class=ORJSONResponse,
 )
 
+@app.on_event('startup')
+async def startup() -> None:
+    """
+    # Подключаемся к базам при старте сервера
+    # Подключиться можем при работающем event-loop
+    # Поэтому логика подключения происходит в асинхронной функции
+
+    :return:
+    """
+    redis.redis = await aioredis.create_redis_pool(
+        (config.REDIS_HOST, config.REDIS_PORT), minsize=10, maxsize=20
+    )
+    elastic.es = AsyncElasticsearch(
+        hosts=[f'{config.ELASTIC_HOST}:{config.ELASTIC_PORT}']
+    )
+
+
+@app.on_event('shutdown')
+async def shutdown():
+    """ Отключаемся от баз при выключении сервера."""
+    await redis.redis.close()
+    await elastic.es.close()
+
 
 if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
         port=8000,
+        log_config=LOGGING,
+        log_level=logging.DEBUG,
     )
 
