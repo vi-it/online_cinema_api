2a3887e7b18c46674ea1615e2655b63a5683a4fb
vitaly <v.shchurov@yadro.com>
2022-11-01 11:51:09 +0300

    К модели Film в приложении добавлены поля

--- src/models/film.py
+++ src/models/film.py
@@ -10,7 +10,16 @@ def orjson_dumps(v, *, default):
 class Film(BaseModel):
     id: str
     title: str
-    description: str
+    description: str | None
+    creation_date: str | None
+    idmb_rating: float | None
+    type: str | None
+    director: str | None
+    actors_name: str | None
+    writers_name: str | None
+    actors: str | None
+    writers: str | None
+    genre: str | None
 
     class Config:
         """Заменяем стандартную работу с json на более быструю."""
