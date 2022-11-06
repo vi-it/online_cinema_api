296108c5b8e7f9b4136053b08180fe842f5196f4
vitaly <v.shchurov@yadro.com>
2022-11-06 19:42:50 +0300

    Changed port to 8001

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ if __name__ == '__main__':
     uvicorn.run(
         'main:app',
         host='0.0.0.0',
-        port=8000,
+        port=8001,
         log_config=LOGGING,
         log_level=logging.DEBUG,
     )
