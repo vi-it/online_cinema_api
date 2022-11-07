bea20ef77e112d588c094fb004c6350b84c1b679
vitaly <v.shchurov@yadro.com>
2022-11-07 10:35:22 +0300

    Added get_many_objects method to the API

--- src/api/v1/films.py
+++ src/api/v1/films.py
@@ -11,18 +11,29 @@ from src.services.service_elt import get_elt_service
 router = APIRouter()
 
 
+@router.get('/film/', response_model=list[models.Film])
+@router.get('/genre/', response_model=list[models.Genre])
+@router.get('/person/', response_model=list[models.Person])
+async def get_many_objects(
+        request: Request,
+        service: ELTService = Depends(get_elt_service)
+):
+    obj = await service.get_many(str(request.url))
+    return obj
+
+
 @router.get('/film/{film_id}', response_model=models.Film)
 @router.get('/genre/{genre_id}', response_model=models.Genre)
 @router.get('/person/{person_id}', response_model=models.Person)
-async def get_obj_by_id(
+async def get_object_by_id(
         obj_id: str,
         request: Request,
         service: ELTService = Depends(get_elt_service)
 ):
-
     obj = await service.get_by_id(obj_id, str(request.url))
     if not obj:
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
-                            detail='film not found')
+                            detail='Object not found')
     return obj
+
 
