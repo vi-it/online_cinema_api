223c2168b54dac3fae7724276668ac82f9625cfb
vitaly <v.shchurov@yadro.com>
2022-11-19 21:20:10 +0300

    tests/functional/utils/wait_for_es.py
    Refactor settings and the module

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -1,13 +1,25 @@
-import os
+"""
+This module is used by Docker Compose to find out if Elasticsearch is okay.
+"""
+
 import time
 
 from elasticsearch import Elasticsearch
+from tests.functional.settings import test_settings
 
-if __name__ == '__main__':
-    host = os.getenv('ELASTIC_HOST', default='127.0.0.1')
-    port = os.getenv('ELASTIC_PORT', default='9200')
-    es_client = Elasticsearch(hosts=f'http://{host}:{port}', validate_cert=False, use_ssl=False)
+
+def wait_elasticsearch():
+    """
+    Wait until Elasticsearch answers to ping(). The function is used to
+    check if Elasticsearch is okay.
+    """
+    hosts = f'http://{test_settings.es_host}:{test_settings.es_port}'
+    es_client = Elasticsearch(hosts=hosts, validate_cert=False, use_ssl=False)
     while True:
         if es_client.ping():
             break
         time.sleep(1)
+
+
+if __name__ == '__main__':
+    wait_elasticsearch()
