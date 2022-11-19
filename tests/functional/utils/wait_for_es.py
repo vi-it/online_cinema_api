5d071f0967e76c8b3823f0d1d960c588f51c90b2
vitaly <v.shchurov@yadro.com>
2022-11-20 00:51:19 +0300

    tests/functional/utils
    Returned using os.getenv instead of TestSettings cause calling the standalone scripts in utils causes ImportError

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -2,10 +2,10 @@
 This module is used by Docker Compose to find out if Elasticsearch is okay.
 """
 
+import os
 import time
 
 from elasticsearch import Elasticsearch
-from tests.functional.settings import test_settings
 
 
 def wait_elasticsearch():
@@ -13,8 +13,15 @@ def wait_elasticsearch():
     Wait until Elasticsearch answers to ping(). The function is used to
     check if Elasticsearch is okay.
     """
-    hosts = f'http://{test_settings.es_host}:{test_settings.es_port}'
-    es_client = Elasticsearch(hosts=hosts, validate_cert=False, use_ssl=False)
+    # os.getenv is used instead of pydantic's settings cause the module
+    # is run a stand-alone scriptf
+    host = os.getenv('ELASTIC_HOST', default='127.0.0.1')
+    port = os.getenv('ELASTIC_PORT', default='9200')
+    es_client = Elasticsearch(
+        hosts=f'http://{host}:{port}',
+        validate_cert=False,
+        use_ssl=False,
+    )
     while True:
         if es_client.ping():
             break
--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,11 +1,11 @@
 """
 This module is used by Docker Compose to find out if Redis is okay.
 """
+
 import asyncio
+import os
 
 import aioredis
-
-from tests.functional.settings import test_settings
 
 
 async def wait_redis():
@@ -13,12 +13,15 @@ async def wait_redis():
     Wait until Redis answers to ping(). The function is used to
     check if Redis is okay.
     """
-    redis = await aioredis.create_redis(f'redis://{test_settings.redis_host}')
+    # os.getenv is used instead of pydantic's settings cause the module
+    # is run a stand-alone script
+    host = os.getenv('REDIS_HOST', '127.0.0.1')
+    redis = await aioredis.create_redis(f'redis://{host}')
     response = await redis.ping()
     while not response:
         await asyncio.sleep(2)
         response = await redis.ping()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(wait_redis())
