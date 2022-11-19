87ba1c337db17297a8f818831ae9c6210972caee
vitaly <v.shchurov@yadro.com>
2022-11-19 21:41:41 +0300

    tests/functional/utils/wait_for_redis.py
    Refactor settings and module

--- tests/functional/utils/wait_for_redis.py
+++ tests/functional/utils/wait_for_redis.py
@@ -1,16 +1,24 @@
+"""
+This module is used by Docker Compose to find out if Redis is okay.
+"""
 import asyncio
-import os
 
 import aioredis
 
+from tests.functional.settings import test_settings
+
 
 async def wait_redis():
-    host = os.getenv("REDIS_HOST", "127.0.0.1")
-    redis = await aioredis.create_redis(f'redis://{host}')
+    """
+    Wait until Redis answers to ping(). The function is used to
+    check if Redis is okay.
+    """
+    redis = await aioredis.create_redis(f'redis://{test_settings.redis_host}')
     response = await redis.ping()
     while not response:
         await asyncio.sleep(2)
         response = await redis.ping()
+
 
 if __name__ == "__main__":
     asyncio.run(wait_redis())
