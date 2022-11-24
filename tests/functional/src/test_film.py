c3b84715e8cbf4cf25f9cd60a45b5c98c15fa066
vitaly <v.shchurov@yadro.com>
2022-11-25 01:37:44 +0300

    tests/functional/src/test_film.py
    Rewrite TestFilmApi.test_get_list

--- tests/functional/src/test_film.py
+++ tests/functional/src/test_film.py
@@ -1,28 +1,50 @@
-# import http
-#
-# import pytest
-#
-# from ..factories.film_factory import FilmFactory
-# from pytest_factoryboy import register
-# from tests.functional.settings import test_settings
-#
-# register(FilmFactory)
-#
-# @pytest.mark.asyncio
-# # @pytest.mark.usefixtures('init_es_film_index')
-# class TestFilmApi:
-#     """"""
-#
-#     async def test_all_films(self, es_write_data, make_get_request, film_factory):
-#
-#         films = film_factory.create_batch(2)
-#         await es_write_data([film.dict() for film in films], 'films',
-#                             test_settings.es_id_field)
-#
-#         response = await make_get_request('films', None)
-#         body = await response.json()
-#
-#         assert response.status == http.HTTPStatus.OK
-#         assert len(body) > 1
-#
-#
+"""
+This module tests API that handles film data.
+"""
+
+import http
+
+import pytest
+
+from tests.functional.models import Film
+from tests.functional.settings import test_settings
+
+
+@pytest.mark.asyncio
+class TestFilmApi:
+    """Test API that handles films data."""
+
+    async def test_get_list(
+        self,
+        storages_clean,
+        create_es_index,
+        es_write_data,
+        make_get_request,
+        films_factory,
+    ):
+        """Test GET films list at /api/v1/films/."""
+        # Setup #
+        await storages_clean(index_name=test_settings.es_index_movies)
+
+        quantity = 3
+        films = films_factory.create_batch(quantity)
+        films_num = len(films)
+
+        create_es_index(index_name=test_settings.es_index_movies)
+        await es_write_data(
+            [film.dict() for film in films],
+            test_settings.es_index_movies,
+            test_settings.es_id_field,
+        )
+
+        # Run #
+        response = await make_get_request(url='films/')
+
+        res = sorted([Film(**i) for i in response.body], key=lambda x: x.id)
+        expected = sorted(films, key=lambda x: x.id)
+
+        # Assertions #
+        assert response.status == http.HTTPStatus.OK
+        assert films_num == quantity
+        assert len(response.body) == films_num
+        assert res == expected
