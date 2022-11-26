6089edc35ae421e1ebe263a3abc420b7223a6a98
vitaly <v.shchurov@yadro.com>
2022-11-26 22:15:36 +0300

    Add closing es_client connection + logging

--- tests/functional/utils/wait_for_es.py
+++ tests/functional/utils/wait_for_es.py
@@ -7,6 +7,12 @@ import time
 
 from elasticsearch import Elasticsearch
 
+import logging.config
+
+logging.config.fileConfig(fname='tests/functional/log.conf',
+                          disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
 
 def wait_elasticsearch():
     """
@@ -14,16 +20,21 @@ def wait_elasticsearch():
     check if Elasticsearch is okay.
     """
     # os.getenv is used instead of pydantic's settings cause the module
-    # is run a stand-alone scriptf
+    # is run as a stand-alone script
     host = os.getenv('ELASTIC_HOST', default='127.0.0.1')
     port = os.getenv('ELASTIC_PORT', default='9200')
+    logger.info('Starting a check if Elasticsearch is up...')
     es_client = Elasticsearch(
         hosts=f'http://{host}:{port}',
         validate_cert=False,
         use_ssl=False,
     )
+    attempt = 1
     while True:
+        logger.info(f'Trying to ping Elasticsearch (attempt {attempt})...')
         if es_client.ping():
+            logger.info(f'Elasticsearch is up!')
+            es_client.close()
             break
         time.sleep(1)
 
